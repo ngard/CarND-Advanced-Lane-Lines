778e41aee0f96d885a80527130ff50fbb0b50c7e
Tohge Norihito <dimension8300@gmail.com>
2017-09-29 21:38:25 +0900

    Added first program which can complete the project_movie.

--- src/detect_edge.py
+++ src/detect_edge.py
@@ -45,20 +45,63 @@ def detect_edge(img_gray):
     return np.absolute(sobelx)
 
 def binary_edge(img_edge):
-    threashold_min = 20
+    threashold_min = 5
     sxbinary = np.zeros_like(img_edge)
     sxbinary[img_edge >= threashold_min] = 255
     return sxbinary
 
 def binary_saturation(img_saturation):
-    threashold_min = 170
-    threashold_max = 500
+    threashold_min = 11
     s_binary = np.zeros_like(img_saturation)
-    s_binary[(img_saturation >= threashold_min) & (img_saturation <= threashold_max)] = 255
+    s_binary[(img_saturation >= threashold_min)] = 255
     return s_binary
 
+def binary_white(img_warped):
+    threashold_min = 255/2
+    white_binary = np.zeros_like(img_warped[:,:,0])
+    white_binary[(img_warped[:,:,0] >= threashold_min) &
+                 (img_warped[:,:,1] >= threashold_min) &
+                 (img_warped[:,:,2] >= threashold_min)] = 255
+    return white_binary
+
+def binary_yellow(img_warped):
+    threashold = 255/2
+    yellow_binary = np.zeros_like(img_warped[:,:,0])
+    yellow_binary[(img_warped[:,:,0] <= threashold) &
+                  (img_warped[:,:,1] >= threashold) &
+                  (img_warped[:,:,2] >= threashold)] = 255
+    return yellow_binary
+
+def binary_white_or_yellow(img_warped):
+    yellow_binary = binary_yellow(img_warped)
+    white_binary = binary_white(img_warped)
+    yw_binary = np.zeros_like(img_warped[:,:,0])
+    yw_binary[(yellow_binary==255) | (white_binary==255)] = 255
+    return yw_binary
+
 def mix_binary_images(img_binary_edge, img_binary_saturation):
+    assert img_binary_edge.size == img_binary_saturation.size
     return np.dstack((np.zeros_like(img_binary_saturation), img_binary_edge, img_binary_saturation))
+
+def mix_binary_images_3(img_binary_edge, img_binary_saturation, img_binary_color):
+    assert img_binary_edge.size == img_binary_saturation.size
+    assert img_binary_edge.size == img_binary_color.size
+    return np.dstack((img_binary_color, img_binary_edge, img_binary_saturation))
+
+def combine_binary_images(img_binary_edge, img_binary_saturation):
+    assert img_binary_edge.size == img_binary_saturation.size
+    combined_binary = np.zeros_like(img_binary_edge)
+    combined_binary[(img_binary_edge == 255) & (img_binary_saturation == 255)] = 255
+    return combined_binary
+
+def combine_binary_images_3(img_binary_edge, img_binary_saturation, img_binary_white_or_yellow):
+    assert img_binary_edge.size == img_binary_saturation.size
+    assert img_binary_edge.size == img_binary_white_or_yellow.size
+    combined_binary = np.zeros_like(img_binary_edge)
+    combined_binary[(img_binary_edge == 255) &
+                    (img_binary_saturation == 255) &
+                    (img_binary_white_or_yellow == 255)] = 255
+    return combined_binary
 
 def undistort_test_images():
     generator_image = read_images("../test_images/")
@@ -107,11 +150,8 @@ def undistort_and_warp_and_detect_edge_on_test_images(M):
         cv2.imwrite(target_dir_mix+fname,img_mix)
 
 def undistort_and_warp_and_detect_edge_on_movie(M,movie_path=""):
-    for img in read_movie(movie_path):
-        cv2.imshow("original",img)        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        img_undist = undistort_image(img,mtx,dist)
+    for img_original in read_movie(movie_path):
+        img_undist = undistort_image(img_original,mtx,dist)
         img_warped = warp_image(img_undist,M)
         img_warped_gray = grayscale_image(img_warped)
         img_warped_edge = detect_edge(img_warped_gray)
@@ -120,9 +160,175 @@ def undistort_and_warp_and_detect_edge_on_movie(M,movie_path=""):
         img_warped_saturation = saturation_image(img_warped)
         img_binary_saturation = binary_saturation(img_warped_saturation)
 
-        img_mix = mix_binary_images(img_binary_edge,img_binary_saturation)
-        cv2.imshow("edge",img_mix)
-        cv2.waitKey(1)
+        img_binary_white_or_yellow = binary_white_or_yellow(img_warped)
+
+        #img_mix = mix_binary_images(img_binary_edge,img_binary_saturation)
+        img_mix = mix_binary_images_3(img_binary_edge,img_binary_saturation,img_binary_white_or_yellow)
+        img_combined = combine_binary_images(img_binary_edge,img_binary_saturation)
+        #img_combined = combine_binary_images_3(img_binary_edge,img_binary_saturation,img_binary_white_or_yellow)
+        yield img_original, img_warped, img_mix, img_combined
+        
+def show_lane_on_movie(M,movie_path):
+    for img_original, img_warped, img_mix, img_combined in undistort_and_warp_and_detect_edge_on_movie(M,movie_path):
+        cv2.imshow("original",img_original)
+        cv2.imshow("warped",img_warped)
+        cv2.imshow("mix",img_mix)
+        cv2.imshow("combined",img_combined)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+def make_sliding_window(binary_warped):
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(binary_warped[binary_warped.shape[0]/2:,:], axis=0)
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 10
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 10
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height
+        win_y_high = binary_warped.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+                      (0,255,0), 2)
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+                      (0,255,0), 2)
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                          (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                           (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    return out_img, left_fit, right_fit
+
+def fit_line(binary_warped, left_fit, right_fit):
+    # Assume you now have a new warped binary image
+    # from the next frame of video (also called "binary_warped")
+    # It's now much easier to find line pixels!
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] - margin)) &
+                      (nonzerox < (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + left_fit[2] + margin)))
+    right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] - margin))&
+                       (nonzerox < (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + right_fit[2] + margin)))
+
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    # Create an image to draw on and an image to show the selection window
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+    left_line_pts = np.array(zip(left_fitx, ploty), np.int32)
+    right_line_pts = np.array(zip(right_fitx, ploty), np.int32)
+    cv2.polylines(window_img, [left_line_pts], False, (255,255,255),1)
+    cv2.polylines(window_img, [right_line_pts], False, (255,255,255),1)
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    return result, left_fit, right_fit
+    
+def show_sliding_window_on_movie(M,movie_path):
+    is_first = True
+    left_fit = 0
+    right_fit = 0
+    for img_original, img_warped, img_mix, img_combined in undistort_and_warp_and_detect_edge_on_movie(M,movie_path):
+        cv2.imshow("original",img_original)
+        cv2.imshow("warped",img_warped)
+        cv2.imshow("mix",img_mix)
+        if is_first:
+            img, left_fit, right_fit = make_sliding_window(img_combined)
+            cv2.imshow("sw",img)
+            is_first = False
+        else:
+            img, left_fit, right_fit = fit_line(img_combined, left_fit, right_fit)
+            cv2.imshow("sw",img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
         
 mtx = np.load("matrix.npy")
 dist = np.load("distortion_coeffs.npy")
@@ -132,12 +338,18 @@ print(mtx,dist)
 
 src = np.float32([[598,450],[685,450],[280,680],[1050,680]])
 dst = np.float32([[300,0],[1000,0],[300,720],[1000,720]])
+#dst = np.float32([[300,0],[1000,0],[300,1000],[1000,1000]])
 M = calc_transform_matrix(src,dst)
 
 #undistort_and_warp_test_images(M)
 
-undistort_and_warp_and_detect_edge_on_test_images(M)
+#undistort_and_warp_and_detect_edge_on_test_images(M)
 
-#undistort_and_warp_and_detect_edge_on_movie(M,"../project_video.mp4")
+video = "../project_video.mp4"
+#video = "../challenge_video.mp4"
+
+#show_lane_on_movie(M,video)
+
+show_sliding_window_on_movie(M,video)
 
 cv2.destroyAllWindows()
