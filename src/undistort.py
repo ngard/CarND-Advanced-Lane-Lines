d67a1275e77f0dca94514d7bc197a8407685808f
Tohge Norihito <dimension8300@gmail.com>
2017-09-30 16:18:36 +0900

    Add writeup Add comments on undistort.py Remove unnecessary lines in undistort.py

--- src/undistort.py
+++ src/undistort.py
@@ -10,7 +10,7 @@ def read_images():
     fname_images = os.listdir(image_dir)
     for fname_image in fname_images:
         img = cv2.imread(image_dir+fname_image)
-        yield img
+        yield img, fname_image
         
 def find_corners(img):
     # Convert to grayscale
@@ -24,17 +24,13 @@ def draw_corners(img,corners):
     return cv2.drawChessboardCorners(img, (nx, ny), corners, True)
 
 def calc_camera_matrix(img, imgpoints):
-    # Use cv2.calibrateCamera() and cv2.undistort()
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    square_size = 5
-
+    # Generate object points corresponded to nx and ny
     objpoints = []
     for points in imgpoints:
         objpoints.append(np.zeros((nx*ny,3), np.float32))
         objpoints[-1][:,:2] = np.mgrid[0:nx,0:ny].T.reshape(-1,2)
-        objpoints[-1] *= square_size
-    
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img.shape[1::-1], None, None)
     return mtx,dist
 
 def undistort(img, mtx, dist):
@@ -44,7 +40,10 @@ def undistort(img, mtx, dist):
 generator_image_1 = read_images()
 generator_image_2 = read_images()
 imgpoints = []
-for img in generator_image_1:
+
+# Here, this program reads images of chessboard patterns,
+# finds corners and saves them to imgpoints.
+for img,fname in generator_image_1:
     corners = find_corners(img)
     if corners is None:
         continue
@@ -53,16 +52,25 @@ for img in generator_image_1:
     cv2.imshow("",img)
     cv2.waitKey(200)
 
+# The line below calculates Camera Matrix and Distortion Coeffs
+# from the imgpoints which have been found above.
 mtx,dist = calc_camera_matrix(img,imgpoints)
-print(mtx,dist)
+print("Camera Matrix",mtx)
+print("Distortion Coeffs",dist)
 
-for img in generator_image_2:
+# The lines below undistorts the chessboard patters
+# with Camera Maxrix and Distortion Coeffs which have been calcurated above.
+# Then shows and saves them.
+for img, fname in generator_image_2:
     cv2.imshow("",img)
     cv2.waitKey(100)
     img_undist = undistort(img,mtx,dist)
     cv2.imshow("",img_undist)
+    cv2.imwrite("../output_images/undistort/"+fname,img_undist)
     cv2.waitKey(200)
 cv2.destroyAllWindows()
 
+# The lines below save calcurated Camera Matrix and Distortion Coeffs to npy file
+# to use them in other programs.
 np.save("matrix.npy",mtx)
 np.save("distortion_coeffs.npy",dist)
